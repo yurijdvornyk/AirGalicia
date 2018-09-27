10a1c31e7ff53bebd714e0630027baafe24494c5
Yurii Dvornyk <yurijdvornyk@gmail.com>
2018-09-27 22:28:22 +0300

    Fix parsing airports array.

--- AirGalicia/Model/Airport.swift
+++ AirGalicia/Model/Airport.swift
@@ -13,5 +13,20 @@ struct Airport: Decodable {
     let city: String
     let country: String
     let name: String
+    let location: String
     let planes: [String]?
+    
+    private enum CodingKeys: String, CodingKey {
+        case code, city, country, name, location, planes
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        code = try container.decode(String.self, forKey: .code)
+        city = try container.decode(String.self, forKey: .city)
+        country = try container.decode(String.self, forKey: .country)
+        name = try container.decode(String.self, forKey: .name)
+        location = try container.decode(String.self, forKey: .location)
+        planes = try container.decode([String].self, forKey: .planes)
+    }
 }
