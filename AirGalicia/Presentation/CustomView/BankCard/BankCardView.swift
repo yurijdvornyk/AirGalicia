9d945162332ddfeec1effcea1a15b8668862b10b
Yurii Dvornyk <yurijdvornyk@gmail.com>
2018-12-16 15:37:51 +0200

    Implement bank card number validation.

--- AirGalicia/Presentation/CustomView/BankCard/BankCardView.swift
+++ AirGalicia/Presentation/CustomView/BankCard/BankCardView.swift
@@ -22,17 +22,41 @@ class BankCardView: UIView {
         super.init(frame: frame)
         commonInit()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
-
+    
     func commonInit() {
         Bundle.main.loadNibNamed(BANK_CARD_VIEW_NIB_NAME, owner: self, options: nil)
         contentView.fixInView(self)
+        cardNumberField.addTarget(self, action: #selector(didChangeValue(textField:)), for: .editingChanged)
+    }
+    
+    @objc func didChangeValue(textField: UITextField) {
+        textField.text = modifyCreditCardString(creditCardString: textField.text!)
+    }
+    
+    func modifyCreditCardString(creditCardString : String) -> String {
+        let trimmedString = creditCardString.components(separatedBy: .whitespaces).joined()
+        
+        let arrOfCharacters = Array(trimmedString.characters)
+        
+        var modifiedCreditCardString = ""
+        
+        if(arrOfCharacters.count > 0) {
+            for i in 0...arrOfCharacters.count - 1{
+                modifiedCreditCardString.append(arrOfCharacters[i])
+                if ((i + 1) % 4 == 0 && i + 1 != arrOfCharacters.count) {
+                    modifiedCreditCardString.append(" ")
+                }
+            }
+        }
+        return modifiedCreditCardString
     }
 }
+
 extension UIView {
     func fixInView(_ container: UIView!) -> Void{
         self.translatesAutoresizingMaskIntoConstraints = false;
@@ -42,5 +66,13 @@ extension UIView {
         NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
         NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
         NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
+
+extension BankCardView: UITextFieldDelegate {
+        
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newLength = textField.text!.characters.count + string.characters.count - range.length
+        return textField == cardNumberField ? newLength <= 19 : true
     }
 }
